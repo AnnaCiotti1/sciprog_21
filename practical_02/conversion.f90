a6280a14075db39e8e25da4cb944e57736ccdf79
AnnaCiotti1 <ciottia@tcd.ie>
2021-10-09 09:53:44 +0100

    Upload of the code conversion.f90 as part of practical 2

--- practical_02/conversion.f90
+++ practical_02/conversion.f90
@@ -1,6 +1,7 @@
 program conversion
 ! Declare variables 
    integer (kind=4) :: i,inum,tmp,numdigits
+   integer::binnum(60)
    real(kind=4) :: fnum
 
 ! Intialise 4-byte integer
@@ -8,6 +9,18 @@ program conversion
 ! Convert to 4-byte float
    fnum = real(inum)       
    
+! Binary converter using modulus   
+   i=1
+   tmp=inum
+   do while (tmp > 0)
+      if(mod(tmp,2)==0) then
+        binnum(i) = 0
+        else
+             binnum(i) = 1
+      end if
+      tmp = tmp/2
+      i = i+1
+   end do
 
 ! Complete the expression
   numdigits = nint(log(fnum)/log(2.0)) 
