996b8bd75fbb894d1b7133678e3f940f9923e991
AnnaCiotti1 <ciottia@tcd.ie>
2021-11-01 18:31:22 +0000

    Correction of the number of steps

--- practical_05/hyperbolic_tangent.f90
+++ practical_05/hyperbolic_tangent.f90
@@ -35,7 +35,7 @@ use tangents
 implicit none
 real(kind=4) :: delta, x
 integer(kind=4) :: i
-real(kind=4) :: array_mac(18), array_log(18)
+real(kind=4) :: array_mac(181), array_log(181)
 
 !Attribution of delta to a positive real number smaller than one chosen by the user
 write(6,*) 'Enter a positive number smaller than 1'
@@ -45,10 +45,10 @@ read(5,*) delta
 x = -0.9
 
 !Calculation of the MacLaurin and logarithmic value of the tangent using the corresponding function for each x, and storage of the results in array_mac and array_log
-do i = 1, 18
+do i = 1, 181
    array_mac(i) = mac_laurin(x, delta)
    array_log(i) = logarithm(x)
-   x = x + 0.1
+   x = x + 0.01
 end do
 
 print *, 'The MacLaurin results for x comprised between -0.9 and 0.9, with a variation of 0.01, are ', array_mac
