fda816c47e71ec926b9f99dae9c4b07c6f3f837e
AnnaCiotti1 <91946660+AnnaCiotti1@users.noreply.github.com>
2021-11-20 10:41:49 +0000

    Addition of implicit none to the subroutine and improving of the comments for the code fibonacci.f90

--- practical_05/fibonacci.f90
+++ practical_05/fibonacci.f90
@@ -1,10 +1,11 @@
-!Creation of a module that contains the subroutine for the fibonacci series
+!Module with the subroutine for the fibonacci series
 module fibonacci_series_creation
 
 contains
 
 !Creation of the subroutine for the fibonacci series
-  subroutine fib(input, output)
+subroutine fib(input, output)
+    implicit none
     integer (kind=4) :: i
     integer(kind=4), intent(in) :: input
     integer, dimension(:), allocatable, intent(out) :: output
@@ -14,12 +15,12 @@ contains
     do i=3, input
       output(i) = output(i-1) + output(i-2)
     end do
-    print *, output
+    write (6,*) 'The fibonacci series of ', input, ' is ', output
 end subroutine fib
 
 end module fibonacci_series_creation
 
-!Start of the main code block
+!Main code block
 program fibonacci
 
 !Import of the module for the use of the subroutine to create the fibonacci series
